864ca23c60771b3ad99deff8b2502d51ba477015
Hakan Tekgül <tekgul2@illinois.edu>
2018-11-27 23:04:36 -0600

    metal classical blues and pop works

--- classifier_music.py
+++ classifier_music.py
@@ -41,10 +41,14 @@ def log_prob(X,g):
 
 classical_path = 'genres/classical'
 metal_path = 'genres/metal'
+blues_path = 'genres/blues'
+pop_path = 'genres/pop'
 
 
 data_classical = []
 data_metal = []
+data_blues = []
+data_pop = []
 sampling_rate = 22050
 num_tracks = 100
 
@@ -71,9 +75,31 @@ for file in os.listdir(metal_path):
     #print(metal.shape)
     data_metal.append(metal)
 
+for file in os.listdir(blues_path):
+    path = os.path.join(blues_path,file)
+    fs,blues = wavread(path)
+    blues = blues.astype(float)
+    #blues = np.pad(blues, (0, 700000 - blues.shape[0]), 'constant', constant_values=(1, 1))
+    shape = blues.shape[0] - 500000
+    blues = blues[:-shape]
+    #print(blues.shape)
+    data_blues.append(blues)
+
+for file in os.listdir(pop_path):
+    path = os.path.join(pop_path,file)
+    fs,pop = wavread(path)
+    pop = pop.astype(float)
+    #pop = np.pad(pop, (0, 700000 - pop.shape[0]), 'constant', constant_values=(1, 1))
+    shape = pop.shape[0] - 500000
+    pop = pop[:-shape]
+    #print(pop.shape)
+    data_pop.append(pop)
+
 data_classical = np.array(data_classical)
 data_metal = np.array(data_metal)
-print(data_metal.shape,data_classical.shape)
+data_blues = np.array(data_blues)
+data_pop = np.array(data_pop)
+print(data_metal.shape,data_classical.shape,data_blues.shape,data_pop.shape)
 
 # Get spectogram features with log and magnitude
 f,t,Zxx_classical = signal.stft(data_classical, fs = sampling_rate, window = 'hann', nperseg = 1024, noverlap=768)
@@ -84,30 +110,63 @@ f,t,Zxx_metal = signal.stft(data_metal, fs = sampling_rate, window = 'hann', npe
 Zxx_metal = np.abs(Zxx_metal)
 Zxx_metal = np.log(Zxx_metal)
 
+f,t,Zxx_blues = signal.stft(data_blues, fs = sampling_rate, window = 'hann', nperseg = 1024, noverlap=768)
+Zxx_blues = np.abs(Zxx_blues)
+Zxx_blues = np.log(Zxx_blues)
+
+f,t,Zxx_pop = signal.stft(data_pop, fs = sampling_rate, window = 'hann', nperseg = 1024, noverlap=768)
+Zxx_pop = np.abs(Zxx_pop)
+Zxx_pop = np.log(Zxx_pop)
+
 mix = np.arange(0, num_tracks, 1) # for random sampling of data
 np.random.shuffle(mix)
 
 Train_classical = np.hstack(Zxx_classical[mix[10:]]) # random 90% of classical data sampled
 Train_metal = np.hstack(Zxx_metal[mix[10:]]) # random 90% of metal data sampled
+Train_blues = np.hstack(Zxx_blues[mix[10:]]) # random 90% of metal data sampled
+Train_pop = np.hstack(Zxx_pop[mix[10:]]) # random 90% of metal data sampled
+
 Test_classical = np.hstack(Zxx_classical[mix[:10]]) # random remaining 10% is for testing
 Test_metal = np.hstack(Zxx_metal[mix[:10]])
+Test_blues = np.hstack(Zxx_blues[mix[:10]])
+Test_pop = np.hstack(Zxx_pop[mix[:10]])
 
-X_train = np.hstack((Train_classical,Train_metal)) # stack'em
-X_test = np.hstack((Test_classical,Test_metal)) #stack'em
+
+
+X_train = np.hstack((Train_classical,Train_metal,Train_blues,Train_pop)) # stack'em
+X_test = np.hstack((Test_classical,Test_metal,Test_blues,Test_pop)) #stack'em
+
+print(X_train.shape,X_test.shape)
 
 # Create labels for testing
 # 1 is classical and 0 if metal
+classical_labels = np.ones(Train_classical.shape[1])
+metal_labels = np.zeros(Train_metal.shape[1])
+blues_labels = np.ones(Train_blues.shape[1]) + np.ones(Train_blues.shape[1])
+pop_labels = np.ones(Train_pop.shape[1]) + np.ones(Train_pop.shape[1]) + np.ones(Train_pop.shape[1])
+
+Y_train = np.hstack((classical_labels,metal_labels,blues_labels,pop_labels))
+
 classical_labels = np.ones(Test_classical.shape[1])
 metal_labels = np.zeros(Test_metal.shape[1])
-Y_test = np.hstack((classical_labels,metal_labels))
+blues_labels = np.ones(Test_blues.shape[1]) + np.ones(Test_blues.shape[1])
+pop_labels = np.ones(Test_pop.shape[1]) + np.ones(Test_pop.shape[1]) + np.ones(Test_pop.shape[1])
 
+Y_test = np.hstack((classical_labels,metal_labels,blues_labels,pop_labels))
+
+print('Starting PCA')
 W = PCA(X_train,64)
 
+'''
 Z_classical = W.dot(Train_classical - np.mean(Train_classical,axis=1,keepdims=True))
 gauss_classical = gauss_classifier(Z_classical)
 
 Z_metal = W.dot(Train_metal - np.mean(Train_metal,axis=1,keepdims=True))
 gauss_metal = gauss_classifier(Z_metal)
+
+Z_blues = W.dot(Train_blues - np.mean(Train_blues,axis=1,keepdims=True))
+gauss_blues = gauss_classifier(Z_blues)
+'''
 
 # function to compute classification accuracy
 def classical_accuracy(pred,Y_test):
@@ -118,20 +177,38 @@ def classical_accuracy(pred,Y_test):
 def one_second_result(probs,fps):
     probs = np.convolve(probs,np.ones(fps))
     return probs
+# Accuracy function to compute classifier accuracy 
+def get_acc(pred, y_test):
+    return np.sum(y_test==pred)/len(y_test)*100
 
+Z_train = W.dot(X_train - np.mean(X_train,axis=1,keepdims=True))
 Z_test = W.dot(X_test - np.mean(X_test,axis=1,keepdims=True))
 # Compute likelihoods for classical and music
+'''
 classical_probs = log_prob(Z_test,gauss_classical)
 metal_probs = log_prob(Z_test,gauss_metal)
-probs = np.hstack((classical_probs,metal_probs))
+blues_probs = log_prob(Z_test,gauss_blues)
+'''
+G = [gauss_classifier(Z_train[:,Y_train == j]) for j in [0,1,2,3]]
+probs = [log_prob(Z_test, i) for i in G]
+pred = np.argmax(probs, axis=0)
+print(get_acc(pred,Y_test))
 
 # According to Piazza, we get the 1-sec probs
+'''
 classical_probs = one_second_result(classical_probs,fps=26)
 metal_probs = one_second_result(metal_probs,fps=26)
+blues_probs = one_second_result(blues_probs,fps=26)
+probs = np.hstack((classical_probs,metal_probs,blues_probs))
 # get predictions
-result = (classical_probs > metal_probs).astype(int)
+metal_pred = np.argmax(metal_probs)
+'''
 
 
+
+
+
+'''
 print('The accuracy of the classical classifier is: %f' %(classical_accuracy(result,Y_test)))
 
 print('Testing with some random metal music...')
@@ -168,7 +245,7 @@ if (classical_accuracy(result,np.ones(result.shape[0]))) < 50:
 else: 
     print('I am pretty sure that this is CLASSICAL MUSIC!')
 
-
+'''
 
 
 
