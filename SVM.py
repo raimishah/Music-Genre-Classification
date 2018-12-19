b881e4681ff517d887ffa6c10383899070de84c4
raimishah <raimi8shah@yahoo.com>
2018-12-19 02:53:00 -0600

    svm best

--- SVM.py
+++ SVM.py
@@ -22,6 +22,7 @@ def get_acc(pred, y_test):
 
 def support_vector_machine(X_train,Y_train,X_test,Y_test):
 	svm = SVC()
+	svm=SVC(C=3,kernel='rbf')
 	svm.fit(X_train,Y_train)
 	pred_train = svm.predict(X_train)
 	pred_test = svm.predict(X_test)
